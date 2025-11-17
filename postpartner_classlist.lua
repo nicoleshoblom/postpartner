ae1062dc7fe2de9f6d1902871d126f37fd609a99
Nicole <nicole.shoblom@gmail.com>
2025-11-17 13:39:30 +0900

    remove launch of google chrome

--- postpartner_classlist.lua
+++ postpartner_classlist.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 function M.run()
-    hs.application.launchOrFocus("Google Chrome")
+    -- hs.application.launchOrFocus("Google Chrome")
+
+    -- hs.timer.usleep(1000000)
+    hs.execute('open -na "Google Chrome" --args --new-window --profile-directory="Default" "https://app.classlist.com/school/#/announcements/create"')
 end
 
 return M
